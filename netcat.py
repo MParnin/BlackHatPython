7b3cdfacc0f2e20b1e2d0b7d3764fe9c868d0013
M Parnin <MParnin@protonmail.com>
2023-10-15 13:02:59 -0400

    Chapter 2: Added comments

--- netcat.py
+++ netcat.py
@@ -16,9 +16,11 @@ def execute(cmd):
     return output.decode()
 
 if __name__ == '__main__':
+    # argparse module creates CLI
     parser = argparse.ArgumentParser(
         description='BHP Net Tool',
         formatter_class=argparse.RawDescriptionHelpFormatter,
+        # Examples from --help command
         epilog=textwrap.dedent('''Example:
             netcat.py -t 192.168.1.108 -p 5555 -l -c # command shell
             netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt # upload to file
@@ -26,6 +28,7 @@ if __name__ == '__main__':
             echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135 # echo text to server port 135
             netcat.py -t 192.168.1.108 -p 5555 # connect to server
         '''))
+    # Arguments for program behavior
     parser.add_argument('-c', '--command', action='store_true', help='command shell')
     parser.add_argument('-e', '--execute', help='execute specified command')
     parser.add_argument('-l', '--listen', action='store_true', help='listen')
@@ -33,6 +36,8 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--target', default='192.168.1.203', help='specified IP')
     parser.add_argument('-u', '--upload', help='upload file')
     args = parser.parse_args()
+    # For listener setup, invokes NetCat object with empty buffer string
+    # Otherwise buffer content sent from stdin
     if args.listen:
         buffer = ''
     else:
